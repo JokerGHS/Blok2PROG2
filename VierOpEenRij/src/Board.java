9f74376cfbe3f81703f2d9de73277f2471e09a4f
JokerGHS <71950812+JokerGHS@users.noreply.github.com>
2020-11-17 03:15:36 +0100

    started building the game

---
+++ VierOpEenRij/src/Board.java
@@ -0,0 +1,9 @@
+
+public class Board {
+
+	private String[][] field;
+	
+	
+	
+
+}
--- VierOpEenRij/src/Game.java
+++ VierOpEenRij/src/Game.java
@@ -1,4 +1,43 @@
+import java.util.Scanner;
+
 
 public class Game {
 
+	private Player[] players;
+	private Board board;
+	Scanner read = new  Scanner(System.in);
+	
+	public Game () {
+		
+		board = new Board();
+		
+	}
+	
+	//Data
+	public void start(int aantal) {
+		
+		//Setting up the players 
+		players = new Player[aantal];
+		for(int i = 0; i < aantal; i++) {
+			players[i] = new Player();
+		}
+		
+		
+	}
+	
+	
+	
+	//View
+	public void drawSetup() {
+	
+		System.out.println("Welkom bij 4 op een rij!");
+		for(int i = 0; i < players.length; i++) {
+			System.out.println("Player " + i+1 + " : fill in your name:");
+			String readName = read.nextLine();
+			players[i].setName(readName);
+		}
+		
+	}
+	
+	
 }
