b355d682c623cc2da32452a0e764db383b9dc21e
Simon Tesfahuney <simon.tesfahuney@outlook.com>
2021-01-17 05:18:12 +0100

    first update

--- Reversi/src/Game/Board.java
+++ Reversi/src/Game/Board.java
@@ -3,12 +3,27 @@ package Game;
 import java.util.HashMap;
 
 public class Board {
-
-	HashMap<String, Square> board;
-	String cels[][];
-
-	Board(){
+	//content
+	private HashMap<String, Square> board;
+	
+	
+	public Board(){
 		board = new HashMap<String, Square>();
-		cels = new String[][];
+		//Using char as an counter so i can use it as an key
+		//when it is combined with the int counter
+		for(char x='a'; x < 'i'; x++){
+			
+			for(int y=0; y<8; y++) {
+			//the combination of the first and second counter becomes the key
+			String cell= x + String.valueOf(y);
+			board.put(cell, new Square());
+			}
+		}
+	
 	}
+	
+	public void print() {
+		
+	}
+	
 }
